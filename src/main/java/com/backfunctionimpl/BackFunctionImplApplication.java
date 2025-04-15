5280ca25aec7524669ad4a38278d2f1bf03747fd
Rabbokki <j55267035@gmail.com>
2025-04-15 16:31:58 +0900

    first

---
+++ src/main/java/com/backfunctionimpl/BackFunctionImplApplication.java
@@ -0,0 +1,13 @@
+package com.backfunctionimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFunctionImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackFunctionImplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backfunctionimpl/BackFunctionImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backfunctionimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFunctionImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
