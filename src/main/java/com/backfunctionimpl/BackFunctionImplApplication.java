571505516ab4ef3dbc1f2fc33a8a1acff68d7466
Rabbokki <j55267035@gmail.com>
2025-04-16 10:56:15 +0900

    설정 파일

--- src/main/java/com/backfunctionimpl/BackFunctionImplApplication.java
+++ src/main/java/com/backfunctionimpl/BackFunctionImplApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackFunctionImplApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackFunctionImplApplication.class, args);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~START~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 
 }
