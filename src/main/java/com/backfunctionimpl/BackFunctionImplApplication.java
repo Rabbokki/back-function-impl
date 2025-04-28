a0a1fe29fea35c7783f77d946c891d73549c5413
Rabbokki <j55267035@gmail.com>
2025-04-28 13:08:15 +0900

    fix run

--- src/main/java/com/backfunctionimpl/BackFunctionImplApplication.java
+++ src/main/java/com/backfunctionimpl/BackFunctionImplApplication.java
@@ -1,6 +1,9 @@
 package com.backfunctionimpl;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -9,8 +12,26 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaAuditing
 @EnableTransactionManagement
 public class BackFunctionImplApplication {
+    @Value("${DB_USERNAME:not-found}")
+    private String dbUsername;
+
+    @Value("${DB_PASSWORD:not-found}")
+    private String dbPassword;
+
     public static void main(String[] args) {
+        // 명시적으로 .env 파일 로드
+        Dotenv dotenv = Dotenv.configure()
+                .directory("C:/Users/80419/Documents/back-function-impl")
+                .filename(".env")
+                .load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(BackFunctionImplApplication.class, args);
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~START~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
+    @PostConstruct
+    public void debugEnvVars() {
+        System.out.println("DB_USERNAME: " + dbUsername);
+        System.out.println("DB_PASSWORD: " + dbPassword);
     }
 }
