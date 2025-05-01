8a02c68751aa2cfce50bce2a7d212dc3d85779c6
Rabbokki <j55267035@gmail.com>
2025-05-01 09:57:02 +0900

    fix review

--- src/main/java/com/backfunctionimpl/review/dto/ReviewDto.java
+++ src/main/java/com/backfunctionimpl/review/dto/ReviewDto.java
@@ -1,7 +1,7 @@
 package com.backfunctionimpl.review.dto;
 
 
-import com.backendfunction.review.entity.Review;
+import com.backfunctionimpl.review.entity.Review;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
