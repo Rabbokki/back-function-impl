08f471b5f534b0eb54db57c35ec45a83c6bc878c
Rabbokki <j55267035@gmail.com>
2025-05-01 10:09:28 +0900

    review

--- src/main/java/com/backfunctionimpl/review/dto/ReviewDto.java
+++ src/main/java/com/backfunctionimpl/review/dto/ReviewDto.java
@@ -1,31 +1,31 @@
-package com.backfunctionimpl.review.dto;
-
-
-import com.backfunctionimpl.review.entity.Review;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-public class ReviewDTO {
-    private Long id;
-    private Long postId;
-    private Long accountId;
-    private int rating;
-    private String nickname;
-    private String content;
-
-    public ReviewDTO(Review review) {
-        this.id = review.getId();
-        this.postId = review.getPost().getId();
-        this.accountId = review.getAccount().getId();
-        this.rating = review.getRating();
-        this.nickname = review.getAccount().getNickname();
-        this.content = review.getContent();
-    }
-}
+//package com.backfunctionimpl.review.dto;
+//
+//
+//import com.backfunctionimpl.review.entity.Review;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//public class ReviewDTO {
+//    private Long id;
+//    private Long postId;
+//    private Long accountId;
+//    private int rating;
+//    private String nickname;
+//    private String content;
+//
+//    public ReviewDTO(Review review) {
+//        this.id = review.getId();
+//        this.postId = review.getPost().getId();
+//        this.accountId = review.getAccount().getId();
+//        this.rating = review.getRating();
+//        this.nickname = review.getAccount().getNickname();
+//        this.content = review.getContent();
+//    }
+//}
