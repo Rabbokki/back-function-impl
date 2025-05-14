facfd67eceddfed573d4bfa08774c455d85df730
Hyojin611 <611gywls@gmail.com>
2025-05-14 14:36:38 +0900

    s

--- src/main/java/com/backfunctionimpl/account/entity/Account.java
+++ src/main/java/com/backfunctionimpl/account/entity/Account.java
@@ -33,9 +33,6 @@ public class Account extends BaseEntity {
     private String name;
 
     @Column(nullable = false)
-    private String role;
-
-    @Column(nullable = false)
     private String nickname;
 
     @Column(nullable = false)
@@ -46,6 +43,10 @@ public class Account extends BaseEntity {
 
     private String provider;
     private String providerId;
+
+
+    @Column(nullable = false)
+    private String role;
 
     private String bio;
 
@@ -75,7 +76,6 @@ public class Account extends BaseEntity {
     private int levelExp = 0;  // 기본 0
 
 
-
     //  현재 레벨 계산
     public TravelLevel getLevel() {
         return TravelLevel.findByExp(this.levelExp);
@@ -95,9 +95,5 @@ public class Account extends BaseEntity {
         this.password = "SOCIAL_LOGIN";
     }
 
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Role role = Role.USER; // 기본값 USER
 
 }
