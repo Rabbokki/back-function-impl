66a67ba320bf2857da6901f976130f89d2743dd3
DESKTOP-QK7FCRU\80415 <sanghoyou@gmail.com>
2025-05-23 10:48:44 +0900

    /api

--- src/main/java/com/backfunctionimpl/like/controller/LikeController.java
+++ src/main/java/com/backfunctionimpl/like/controller/LikeController.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/like")
+@RequestMapping("/api/like")
 @RequiredArgsConstructor
 public class LikeController {
     private final LikeService likeService;
